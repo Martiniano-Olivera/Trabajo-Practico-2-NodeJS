093edcfa9bef8307349196ba479b9d9caf8362db
Martiniano <martiniano_olivera@hotmail.com>
2019-11-15 03:58:10 -0300

    first commit.

---
+++ db.js
@@ -0,0 +1,8 @@
+function BBDD(ciudad, humedad, presion, temp) {
+    this.ciudad = ciudad;
+    this.humedad = humedad;
+    this.presion = presion;
+    this.temp = temp;
+}
+
+module.exports = BBDD;
